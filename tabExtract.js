75b552f4f54f051f529c727aa6623d92754e30e1
Monting Lin <monting@pingg.com>
2013-02-10 21:08:40 -0500

    style corrections

--- tabExtract.js
+++ tabExtract.js
@@ -7,43 +7,43 @@
 
   // This event is fired each time the user updates the text in the omnibox,
   // as long as the extension's keyword mode is still active.
-  chrome.omnibox.onInputChanged.addListener( function(text, suggest) {
+  chrome.omnibox.onInputChanged.addListener( function( text, suggest ) {
     //console.log('inputChanged: ' + text);
     entered = false;
 
     // pointer to suggest to be used inside callback function for getWindow
     suggestPtr = suggest;
-    suggestStart(text);
-   } );
+    suggestStart( text );
+  } );
 
   // This event is fired with the user accepts the input in the omnibox.
   chrome.omnibox.onInputEntered.addListener(start);
 
-  function start(text) {
+  function start( text ) {
     entered = true;
     tabsToMove = [];
     keywords = text.toLowerCase().split(" ");
-    chrome.windows.getAll({"populate" : true}, getWindows);
+    chrome.windows.getAll( {"populate" : true}, getWindows );
   }
 
-  function suggestStart(text) {
+  function suggestStart( text ) {
     tabsToMove = [];
     keywords = text.toLowerCase().split(" ");
-    chrome.windows.getAll({"populate" : true}, getWindows);
+    chrome.windows.getAll( {"populate" : true}, getWindows );
   }
 
   //searches for keyword matches in tab titles and url
-  function matchTheKeywords(tab) {
+  function matchTheKeywords( tab ) {
     var numKeywords = keywords.length;
     for (var i = 0; i < numKeywords; i++) {
       var keyw = keywords[i];
-      if ((tab.url.toLowerCase()).search(keywords[i]) > -1) {return true;}
-      if ((tab.title.toLowerCase()).search(keywords[i]) > -1) {return true;}
+      if ( (tab.url.toLowerCase()).search(keywords[i]) > -1 ) { return true; }
+      if ( (tab.title.toLowerCase()).search(keywords[i]) > -1 ) { return true; }
     }
     return false;
   }
 
-  function getWindows(windows) {
+  function getWindows( windows ) {
     var numWindows = windows.length;
 
     //for each window
@@ -56,7 +56,7 @@
         var currTab = win.tabs[j];
         if ( matchTheKeywords(currTab) && !currTab.pinned ) {
           //add to tabsToMove list
-          tabsToMove.push(win.tabs[j]);
+          tabsToMove.push( win.tabs[j] );
         }
 
       }
@@ -64,7 +64,7 @@
 
     // users pressed enter
     if (entered){
-      if (tabsToMove.length < 1 || keywords[0] === "") {
+      if ( tabsToMove.length < 1 || keywords[0] === "" ) {
         alert(
           'no matches found for the keywords "' + keywords + '".'
           + "\nNote: You do not need to press down to select a suggestion. Just press enter after entering keywords."
@@ -72,16 +72,17 @@
       }
       else {
         //create new window and move all tabs in tabsToMove to the new window
-        chrome.windows.create({}, moveTabs);
+        chrome.windows.create( {}, moveTabs );
       }
     }
     // user has not yet pressed enter, make suggestion
     else {
       var descr;
-      if (tabsToMove.length < 1) {
+      if ( tabsToMove.length < 1 ) {
         descr = tabsToMove.length + " tabs matching. Enter another keyword or press ESC to cancel.";
         suggestPtr( [{content: "", description: descr}] );
-      }else {
+      }
+      else {
         descr = tabsToMove.length + " tabs matching. Press enter to move them to a new window.";
         suggestPtr( [{content: "", description: descr}] );
       }
@@ -95,7 +96,7 @@
     }
 
     //remove the empty tab created with window creation
-    chrome.tabs.remove(win.tabs[win.tabs.length - 1].id);
+    chrome.tabs.remove( win.tabs[win.tabs.length - 1].id );
   }
 
 })();
